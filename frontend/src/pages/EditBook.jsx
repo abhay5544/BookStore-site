c5979148d2fb1eef02629568fa22a71cb630de52
abhay5544 <abhaybudheliya59@gmail.com>
2026-03-13 16:59:59 +0530

    frontend update to site

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditBook = () => {
+   const [title, setTitle] = useState('');
+   const [author, setAuthor] = useState('');
+   const [publishYear, setPublishYear] = useState('');
+   const [loading, setLoading] = useState(false);
+   const navigate = useNavigate();
+   const {id} = useParams();
+   useEffect(() => {
+     setLoading(true);
+      axios.get(`http://localhost:5173/books/${id}`)
+      .then((response) => {
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear)
+        setTitle(response.data.title)
+        setLoading(true)
+      }).catch((error) => {
+         setLoading(false);
+         alert('An error happened. Please Check console');
+         console.log(error);
+         
+      })
+   }, [])
+   const handleEditBook = () => {
+     const data = {
+       title,
+       author,
+       publishYear,
+     };
+     setLoading(true);
+     axios
+      .put(`http://localhost:5173/books${id}`, data)
+      .then(() => {
+         setLoading(false);
+         navigate('/')
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happend. Please Che')
+      })
+   };
+
+
   return (
-    <div>EditBook</div>
+    <div>Edit Book</div>
   )
 }
 
 export default EditBook
+
