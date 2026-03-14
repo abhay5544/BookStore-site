b4dc4cfaaa1a8323c129e6bba3328ae054c5a4ad
abhay5544 <abhaybudheliya59@gmail.com>
2026-03-14 22:08:13 +0530

    frontend updating frontpages

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -12,42 +12,80 @@ const EditBook = () => {
    const navigate = useNavigate();
    const {id} = useParams();
    useEffect(() => {
-     setLoading(true);
-      axios.get(`http://localhost:5173/books/${id}`)
-      .then((response) => {
-        setAuthor(response.data.author);
-        setPublishYear(response.data.publishYear)
-        setTitle(response.data.title)
-        setLoading(true)
-      }).catch((error) => {
+       setLoading(true);
+       axios.get(`http://localhost:5555/books${id}`)
+       .then((response) => {
+         setAuthor(response.data.author);
+         setPublishYear(response.data.publishYear)
+         setTitle(response.data.title)
+         setLoading(false)
+         navigate('/')
+       }).catch((error) => {
          setLoading(false);
-         alert('An error happened. Please Check console');
+         alert('An error happened. Please Check console')
          console.log(error);
          
-      })
+       })
    }, [])
-   const handleEditBook = () => {
-     const data = {
-       title,
-       author,
-       publishYear,
-     };
-     setLoading(true);
-     axios
-      .put(`http://localhost:5173/books${id}`, data)
+
+     const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear
+    };
+
+    axios.put(`http://localhost:5555/books${id}`, data)
       .then(() => {
-         setLoading(false);
-         navigate('/')
+        alert("Book Created");
       })
       .catch((error) => {
-        setLoading(false);
-        alert('An error happend. Please Che')
-      })
-   };
-
-
+        console.log(error);
+      });
+  };
+  
   return (
-    <div>Edit Book</div>
+    <div className='p-4'>
+        <BackButton />
+        <h1 className='text-3xl my-4'>Create Book</h1>
+        {loading ? ( 
+            <Spinner />
+        ) : (
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+            <div className='my-4'>
+                <lable className='text-xl mr-4 text-gray-500'>Title</lable>
+                <input 
+                  type='text'
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className='border-2 border-gray-500 px-4 py-2 w-full'
+                />
+            </div>
+            <div className='my-4'>
+                <label className='text-xl mr-4 text-gray-500'>Author</label>
+                <input 
+                  type='text'
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                  className='border-2 border-gray-500 px-4 py-2 w-full'
+                />
+            </div>
+            <div className='my-4'>
+                <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+                <input 
+                  type='text'
+                  value={publishYear}
+                  onChange={(e) => setPublishYear(e.target.value)}
+                  className='border-2 border-gray-500 px-4 py-2 w-full'
+                />
+            </div>
+            <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+              Save
+            </button>
+        </div>
+           
+        )}
+        </div>
   )
 }
 
